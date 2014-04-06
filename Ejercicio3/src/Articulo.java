2a4d8a9625a0a3fe58b8b234f707c24d48009dce
Cesar Tomatis <cesartomatis@gmail.com>
2014-04-06 17:05:13 -0300

    Ejer 3

---
+++ Ejercicio3/src/Articulo.java
@@ -0,0 +1,32 @@
+
+public class Articulo {
+	private int id; 
+	private int stock;
+	private String descripcion;
+	private double precio;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+}
